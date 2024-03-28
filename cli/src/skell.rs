1249e8b42ed90561b80e7f6b508a6d1e811d9566
Vincent Ruello <5345986+vruello@users.noreply.github.com>
2024-03-28 15:16:46 +0100

    Fix minimal skell

--- cli/src/skell.rs
+++ cli/src/skell.rs
@@ -202,7 +202,7 @@ pub fn get_minimal_skell_content(uuid: Uuid, name: &str, now: DateTime<Local>) -
 [[outputs]]
 driver = "Files"
 format = "Raw"
-config = {{ base = "/var/log/openwec/" }}
+config = { base = "/var/log/openwec/" }
 "#,
     );
     content
