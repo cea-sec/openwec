4dd7cb8278dd99baa200c91e19e2cd709b245eff
Vincent Ruello <5345986+vruello@users.noreply.github.com>
2025-09-26 21:04:04 +0200

    Fix code format

--- common/src/subscription.rs
+++ common/src/subscription.rs
@@ -50,7 +50,8 @@ impl KafkaConfiguration {
 
     /// Get the kafka configuration's topic split on commas.
     pub fn topic_as_array(&self) -> Vec<String> {
-        self.topic.split(',')
+        self.topic
+            .split(',')
             .map(|s| s.trim().to_string())
             .filter(|s| !s.is_empty())
             .collect()
